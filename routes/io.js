f7ab22497a1f187f271d862b5e28a494b390c3e9
Satendra Singh <satendra_singh@live.com>
2017-04-22 21:10:48 +0530

    fixed gpio

--- routes/io.js
+++ routes/io.js
@@ -2,28 +2,27 @@ var express = require('express');
 var router = express.Router();
 var gpio = require('rpi-gpio');
  
-router.get('/setup/write/:port',(req, res, next) =>{
-    let port = req.params.port;
-    gpio.setup(7, gpio.DIR_HIGH, write);
-    res.send('done');
-});
-
 router.get('/left/:port', (req, res, next) =>
 {
     let port = req.params.port;
-    gpio.write(port, false, function(err) {
+    gpio.setup(7, gpio.DIR_HIGH,()=>{
+        gpio.write(port, false, function(err) {
         if (err) throw err;
         console.log('Written to pin');
+        });
     });
+
     res.send('done');
 });
 
 router.get('/right/:port', (req, res, next) =>
 {
-    let port = req.params.port;
-    gpio.write(port, false, function(err) {
+   let port = req.params.port;
+    gpio.setup(7, gpio.DIR_HIGH,()=>{
+        gpio.write(port, false, function(err) {
         if (err) throw err;
         console.log('Written to pin');
+        });
     });
 
     res.send('done');
